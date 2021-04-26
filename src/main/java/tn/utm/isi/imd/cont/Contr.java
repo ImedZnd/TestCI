e256ff4612b2c1c4d4e9baec72d41156eda533cd
imed <imedzenned@yahoo.fr>
2021-04-26 12:06:31 +0200

    tyjtyj

---
+++ src/main/java/tn/utm/isi/imd/cont/Contr.java
@@ -0,0 +1,15 @@
+package tn.utm.isi.imd.cont;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Contr {
+	
+	@GetMapping
+	String getTest() {
+		return "Hello World";
+		
+	}
+
+}
